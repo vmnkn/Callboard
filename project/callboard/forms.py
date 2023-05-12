42408e80127af172586c004ec7d506cbca3d5ce7
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-05-12 23:27:06 +0300

    add posts for db

--- project/callboard/forms.py
+++ project/callboard/forms.py
@@ -29,6 +29,9 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Comment
         fields = [
--- project/project/settings.py
+++ project/project/settings.py
@@ -147,3 +147,9 @@ LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/callboard/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/callboard/'
 
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
